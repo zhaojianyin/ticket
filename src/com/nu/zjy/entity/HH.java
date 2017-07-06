2477de422b03b34a927da6f4b3f3d56bb7594c29
zhaojianyin <384144795@qq.com>
2017-07-06 11:18:39 +0800

    Change-Id: Ie7552bb9d0447a79afcf823bd18f4c0e1f678a19 Signed-off-by: zhaojianyin <384144795@qq.com>

---
+++ src/com/nu/zjy/entity/HH.java
@@ -0,0 +1,17 @@
+package com.nu.zjy.entity;
+
+/** 
+ * @项目名称：ticket   
+ * @类名称：HH   
+ * @类描述：  
+ *
+ * @author 赵建银-邮箱：384144795@qq.com 
+ * @date 创建时间：2017-7-6 下午12:14:03 
+ * @version 1.0 
+ * @parameter  
+ * @since  
+ * @return  
+ */
+public class HH {
+
+}
---
+++ src/com/nu/zjy/entity/Ticket.java
@@ -0,0 +1,17 @@
+package com.nu.zjy.entity;
+
+/** 
+ * @项目名称：ticket   
+ * @类名称：Ticket   
+ * @类描述： 车票的实体类
+ *
+ * @author 赵建银-邮箱：384144795@qq.com 
+ * @date 创建时间：2017-7-6 上午11:43:13 
+ * @version 1.0 
+ * @parameter  
+ * @since  
+ * @return  
+ */
+public class Ticket {
+
+}
